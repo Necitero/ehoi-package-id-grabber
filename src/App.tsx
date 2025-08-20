2c530d1a9953a685231bf6e6118c6523b7b49873
Nico Borst <nico.borst@syzygy.de>
2025-08-20 10:14:52 +0200

    fix: prevent date as package name

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,61 @@ interface PackageSet {
   tripId: string;
 }
 
+type E = HTMLElement;
+
+const funcCallback = () => {
+  const pkgSets: PackageSet[] = [];
+  const SUBROW = ".row.termin.sub";
+  const MAINROW = ".row.termin.main";
+  const SUBSTITUTE_NAME = "Paketname konnte nicht gefunden werden.";
+
+  const rows = document.querySelectorAll<E>(SUBROW);
+  const mainRow = document.querySelector<E>(MAINROW);
+  const mainTitle = mainRow?.children[0]?.textContent;
+  if (!rows) return null;
+
+  // High possibility the title is a date if it's only one
+  if (rows.length === 1) {
+    const regex = new RegExp(/([0-9]{2}\.){2}[0-9]{4}/);
+    const title = rows[0].querySelector(".title");
+    const { tripid, packageid } = rows[0].dataset;
+    if (!title || !tripid || !packageid) {
+      return;
+    }
+    const match = regex.exec(title.childNodes[0].textContent || "");
+    let name = "";
+    // If name is date, use main row title
+    if (match) {
+      name = mainTitle || SUBSTITUTE_NAME;
+    } else {
+      name = title.childNodes[0].textContent || "Could ";
+    }
+    pkgSets.push({
+      name,
+      tripId: tripid,
+      packageId: packageid,
+    });
+  }
+
+  for (const row of rows) {
+    const { tripid, packageid } = row.dataset;
+    const title = row.querySelector(".title");
+    const exists = pkgSets.find((pkgSet) => pkgSet.packageId === packageid);
+
+    if (tripid && packageid && title && !exists) {
+      pkgSets.push({
+        name: title.childNodes[0].textContent || SUBSTITUTE_NAME,
+        tripId: tripid,
+        packageId: packageid,
+      });
+    }
+  }
+  return pkgSets;
+};
+
 function App() {
   const [packages, setPackages] = useState<PackageSet[] | null>(null);
   const [active, setActive] = useState<number | null>(null);
-
-  const handleActiveChange = (index: number) => {
-    if (active === index) {
-      setActive(null);
-      return;
-    }
-    setActive(index);
-  };
 
   useEffect(() => {
     (async () => {
@@ -26,35 +70,23 @@ function App() {
         currentWindow: true,
       });
       if (!tab?.id) return;
+
       const [injection] = await chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: () => {
-          const pkgSets: PackageSet[] = [];
-          const rows =
-            document.querySelectorAll<HTMLElement>(".row.termin.sub");
-          if (!rows) return null;
-          for (const row of rows) {
-            const { tripid, packageid } = row.dataset;
-            const title = row.querySelector(".title");
-            const exists = pkgSets.find(
-              (pkgSet) => pkgSet.packageId === packageid,
-            );
-
-            if (tripid && packageid && title && !exists) {
-              pkgSets.push({
-                name: title.childNodes[0].textContent || "",
-                tripId: tripid,
-                packageId: packageid,
-              });
-            }
-          }
-          return pkgSets;
-        },
+        func: funcCallback,
       });
 
       setPackages(injection?.result ?? null);
     })();
   }, []);
+
+  const handleActiveChange = (index: number) => {
+    if (active === index) {
+      setActive(null);
+      return;
+    }
+    setActive(index);
+  };
 
   return (
     <div>
