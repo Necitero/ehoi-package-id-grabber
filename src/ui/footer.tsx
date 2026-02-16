ed54964495c3a98585d76c610ad36c5c8a89d8e5
Necitero <necitero@gmail.com>
2026-02-16 13:19:34 +0100

    refactor(footer,header): move version info to footer

--- src/ui/footer.tsx
+++ src/ui/footer.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
+import {
+  getLatestVersion,
+  getLocalVersion,
+  isVersionLatest,
+} from "../helpers/version";
+
 export const Footer = ({ message }: { message?: string }) => {
   const openGithub = () => {
     chrome.tabs.create({
       url: "https://github.com/Necitero/ehoi-package-id-grabber",
     });
   };
+  const [isLatest, setIsLatest] = useState<boolean>(true);
+  useEffect(() => {
+    getLatestVersion().then((version) => {
+      const localVersion = getLocalVersion();
+      setIsLatest(isVersionLatest(localVersion, version));
+    });
+  }, []);
+
   return (
     <footer className="footer">
       <button className="footer-cta" onClick={openGithub}>
         {message ?? "Made with ♡ by Necitero"}
+        {!isLatest && (
+          <>
+            <br />
+            <span>Neue Version verfügbar</span>
+          </>
+        )}
       </button>
     </footer>
   );
--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,23 +1,7 @@
-import { useEffect, useState } from "react";
-import {
-  getLatestVersion,
-  getLocalVersion,
-  isVersionLatest,
-} from "../helpers/version";
-
 export const Header = () => {
-  const [isLatest, setIsLatest] = useState<boolean>(true);
-  useEffect(() => {
-    getLatestVersion().then((version) => {
-      const localVersion = getLocalVersion();
-      setIsLatest(isVersionLatest(localVersion, version));
-    });
-  }, []);
-
   return (
     <header>
       <h1>ID Grabber</h1>
-      <span>{isLatest ? ":)" : ":("}</span>
       <span className="reload" onClick={() => window.location.reload()}>
         Reload
       </span>
